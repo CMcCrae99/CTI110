14a90bef5047a4e527acb09aac7a626a6a576b14
CMcCrae99 <107217312+CMcCrae99@users.noreply.github.com>
2022-06-09 17:59:35 -0400

    Add files via upload
    Second submission

---
+++ P1HW1_Output_McCrae.py
@@ -0,0 +1,9 @@
+user_num1 = int(input('Enter integer:\n'))
+
+print('You entered:', user_num1)
+print(user_num1, 'squared is', user_num1 * user_num1)
+print('And', user_num1, 'cubed is', user_num1 * user_num1 * user_num1 , '!!')
+print('Enter another integer:')
+user_num2 = int(input(''))
+print(user_num1, '+', user_num2, 'is', user_num1 + user_num2)
+print(user_num1, '*', user_num2, 'is', user_num1 * user_num2)
